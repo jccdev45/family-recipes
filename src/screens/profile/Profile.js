95bbf66c4afc7f7239ecac0a4d78a9ec43e20ceb
JordanCruzCorrea <jccdev45@gmail.com>
2021-05-10 21:03:09 -0400

    Resp. changes

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -8,15 +8,15 @@ export function Profile() {
 
 	return (
 		<AuthContainer>
-			<div className="flex flex-col w-full p-8 m-auto rounded shadow">
+			<div className="flex flex-col w-full p-8 m-auto rounded shadow lg:w-2/3">
 				<h2 className="mb-4 text-3xl font-bold text-center">Profile</h2>
-				<img src={currentUser.photoURL} className="w-1/2 mx-auto rounded-full" alt="" />
-				<span className="text-xl">
+				<img src={currentUser.photoURL} className="w-1/2 mx-auto rounded-full lg:w-1/3" alt="" />
+				<div className="text-xl">
 					<strong>Display Name:</strong> {currentUser.displayName}
-				</span>
-				<span className="text-xl">
+				</div>
+				<div className="text-xl">
 					<strong>Email:</strong> {currentUser.email}
-				</span>
+				</div>
 			</div>
 			<Link
 				to="/update-profile"
