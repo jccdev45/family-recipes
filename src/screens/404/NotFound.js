e63c2538bfe7e99a12381177b199ac5a5afaa67b
JordanCruzCorrea <jccdev45@gmail.com>
2021-05-12 14:29:18 -0400

    Styling fix for 404 img

--- src/screens/404/NotFound.js
+++ src/screens/404/NotFound.js
@@ -1,19 +1,16 @@
 import { Link } from "react-router-dom";
 import FourOhFour from "../../data/img/404.svg";
 import { AuthContainer } from "../../components/auth";
-import { ImgWrapper } from "../../components/shared/ImgWrapper";
 
 export function NotFound() {
 	return (
 		<AuthContainer>
 			<h1 className="text-3xl font-bold">w h o o p s</h1>
-			<ImgWrapper>
-				<img
-					src={FourOhFour}
-					className="w-5/6 mx-auto"
-					alt="404 page not found"
-				/>
-			</ImgWrapper>
+			<img
+				src={FourOhFour}
+				className="w-2/3 mx-auto"
+				alt="404 page not found"
+			/>
 			<h2 className="text-xl">what are you doing here, beat it kid</h2>
 			<Link to="/" className="text-blue-400 hover:underline">
 				return from whence you came
