1ce45a1a8d29f909cdb56a8d0c9cfa5cf1e019f9
JordanCruzCorrea <jccdev45@gmail.com>
2021-05-14 14:22:09 -0400

    Style & banner changes

--- src/components/shared/Hero.js
+++ src/components/shared/Hero.js
@@ -2,12 +2,19 @@ import { Link } from "react-router-dom";
 
 export function Hero({ name, quote, author, img, page }) {
 	return (
+		// <div
+		// 	className={`${img} ${
+		// 		page === "home"
+		// 			? "h-36 bg-top bg-contain md:bg-hero-family lg:h-72 lg:bg-hero-family-lg"
+		// 			: "h-80 md:h-52 lg:h-80 bg-top bg-cover md:bg-top md:bg-contain"
+		// 	} lg:bg-top lg:bg-contain w-screen bg-fixed bg-purple-100 flex justify-between bg-repeat px-8 m-auto`}
+		// >
 		<div
 			className={`${img} ${
 				page === "home"
-					? "h-36 bg-top bg-contain md:bg-hero-family lg:h-72 lg:bg-hero-family-lg"
-					: "h-80 md:h-52 lg:h-80 bg-top bg-cover md:bg-top md:bg-contain"
-			} lg:bg-top lg:bg-contain w-screen bg-fixed bg-purple-100 flex justify-between bg-no-repeat px-8 m-auto`}
+					? "h-36 bg-center bg-contain md:h-56 lg:h-72 bg-repeat lg:bg-repeat-round md:bg-hero-family-lg"
+					: "h-80 md:h-52 lg:h-80 bg-top bg-cover bg-fixed md:bg-top md:bg-contain bg-no-repeat"
+			} lg:bg-top lg:bg-contain w-screen bg-purple-100 flex justify-between px-8 m-auto`}
 		>
 			<div className="w-2/3 p-8 m-auto font-bold text-center text-white bg-gray-800 rounded bg-opacity-70 lg:w-1/3">
 				<h1 className="text-xl text-red-100 md:text-4xl">
