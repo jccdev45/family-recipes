d063a1f85a623ce7684762fa11f2836293231c26
JordanCruzCorrea <jccdev45@gmail.com>
2021-05-19 16:18:21 -0400

    Steps drag n drop/minor changes to form ✅

--- src/components/form/RenderArrayInputs.js
+++ src/components/form/RenderArrayInputs.js
@@ -1,3 +1,4 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import {
 	ADD_VALUE_BTN_CLASS,
 	CHECKMARK_CLASS,
@@ -7,21 +8,62 @@ import {
 	SMALL_CLASS,
 } from "../../data/constants/add_recipe_const";
 
+const reorder = (list, startIndex, endIndex) => {
+	const result = Array.from(list);
+	const [removed] = result.splice(startIndex, 1);
+	result.splice(endIndex, 0, removed);
+
+	return result;
+};
+
+const getListStyle = (isDraggingOver) => ({
+	background: isDraggingOver ? "lightgray" : null,
+});
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+	userSelect: "none",
+	padding: 4,
+	background: isDragging ? "pink" : "",
+
+	...draggableStyle,
+});
+
 export function RenderArrayInputs({
+	error,
 	ing,
 	ingConfirm,
-  recipe,
+	recipe,
+	setRecipe,
 	step,
 	stepConfirm,
 	tag,
 	tagConfirm,
 	violentlyItChanges,
 	addToValue,
-  removeValue
+	removeValue,
 }) {
+	const onDragEnd = (result) => {
+		if (!result.destination) {
+			return;
+		}
+
+		const steps = reorder(
+			recipe.steps,
+			result.source.index,
+			result.destination.index
+		);
+
+		setRecipe((recipe) => {
+			return {
+				...recipe,
+				steps,
+			};
+		});
+	};
 	return (
-		<>
-			<label htmlFor="ing" className="relative flex items-center">
+		<DragDropContext onDragEnd={onDragEnd}>
+			{/* INGREDIENTS */}
+			<label htmlFor="ing" className="relative flex items-center w-full">
 				<small className={SMALL_CLASS}>Ingredient</small>
 				<input
 					className={`${
@@ -39,7 +81,7 @@ export function RenderArrayInputs({
 				<span className={ADD_VALUE_BTN_CLASS}>
 					<input
 						type="button"
-						value="⨁"
+						value="+"
 						className="bg-transparent cursor-pointer"
 						onClick={() => addToValue("ingredients")}
 					/>
@@ -60,7 +102,7 @@ export function RenderArrayInputs({
 							<input
 								type="button"
 								value="X"
-								className={REMOVE_BADGE_CLASS}
+								className={`${REMOVE_BADGE_CLASS} -right-1 top-0`}
 								onClick={() => removeValue("ingredients", ing)}
 							/>
 						</span>
@@ -69,7 +111,9 @@ export function RenderArrayInputs({
 					<div className="w-full h-full p-2 text-center">Pantry</div>
 				)}
 			</ul>
-			<label htmlFor="step" className="relative flex items-center">
+
+			{/* STEPS */}
+			<label htmlFor="step" className="relative flex items-center w-full">
 				<small className={SMALL_CLASS}>Step</small>
 				<input
 					className={`${
@@ -87,7 +131,7 @@ export function RenderArrayInputs({
 				<span className={ADD_VALUE_BTN_CLASS}>
 					<input
 						type="button"
-						value="⨁"
+						value="+"
 						className="bg-transparent cursor-pointer"
 						onClick={() => addToValue("steps")}
 					/>
@@ -100,26 +144,60 @@ export function RenderArrayInputs({
 					✅
 				</span>
 			</label>
-			<ul className="flex flex-col items-center justify-center w-full py-2 my-2 bg-gray-100 rounded-lg">
-				{recipe.steps && recipe.steps.length ? (
-					recipe.steps.map((step, index) => (
-						<span key={index} className="relative">
-							<li className={`${LIST_BADGE_CLASS} list-decimal`}>
-								{step.charAt(0).toUpperCase() + step.slice(1)}
-							</li>
-							<input
-								type="button"
-								value="X"
-								className={REMOVE_BADGE_CLASS}
-								onClick={() => removeValue("steps", step)}
-							/>
-						</span>
-					))
-				) : (
-					<div className="w-full h-full p-2 text-center">Stairs</div>
+			<Droppable droppableId="droppable">
+				{(provided, snapshot) => (
+					<>
+						<h2 className="font-bold text-center text-red-400">
+							Click and drag to reorder steps as needed
+						</h2>
+						<ul
+							{...provided.droppableProps}
+							ref={provided.innerRef}
+							style={getListStyle(snapshot.isDraggingOver)}
+							className="flex flex-col items-center justify-center w-full px-8 py-2 my-2 transition-colors duration-200 ease-in-out bg-gray-100 divide-y-2 divide-gray-300 rounded-lg"
+						>
+							{recipe.steps && recipe.steps.length ? (
+								recipe.steps.map((step, index) => (
+									<Draggable
+										key={index}
+										draggableId={step + index}
+										index={index}
+									>
+										{(provided, snapshot) => (
+											<span
+												className="relative w-11/12 my-1 transition-colors duration-100 ease-in-out bg-gray-200 rounded-lg"
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+												style={getItemStyle(
+													snapshot.isDragging,
+													provided.draggableProps.style
+												)}
+											>
+												<li className={`list-item list-inside list-decimal`}>
+													{step.charAt(0).toUpperCase() + step.slice(1)}
+												</li>
+												<input
+													type="button"
+													value="X"
+													className={`${REMOVE_BADGE_CLASS} -right-8 top-1`}
+													onClick={() => removeValue("steps", step)}
+												/>
+											</span>
+										)}
+									</Draggable>
+								))
+							) : (
+								<div className="w-full h-full p-2 text-center">Stairs</div>
+							)}
+							{provided.placeholder}
+						</ul>
+					</>
 				)}
-			</ul>
-			<label htmlFor="tag" className="relative flex items-center">
+			</Droppable>
+
+			{/* TAGS */}
+			<label htmlFor="tag" className="relative flex items-center w-full">
 				<small className={SMALL_CLASS}>Tag</small>
 				<input
 					className={`${
@@ -137,7 +215,7 @@ export function RenderArrayInputs({
 				<span className={ADD_VALUE_BTN_CLASS}>
 					<input
 						type="button"
-						value="⨁"
+						value="+"
 						className="bg-transparent cursor-pointer"
 						onClick={() => addToValue("tags")}
 					/>
@@ -158,7 +236,7 @@ export function RenderArrayInputs({
 							<input
 								type="button"
 								value="X"
-								className={REMOVE_BADGE_CLASS}
+								className={`${REMOVE_BADGE_CLASS} -right-1 top-0`}
 								onClick={() => removeValue("tags", tag)}
 							/>
 						</span>
@@ -167,6 +245,6 @@ export function RenderArrayInputs({
 					<div className="w-full h-full p-2 text-center">You're it</div>
 				)}
 			</ul>
-		</>
+		</DragDropContext>
 	);
 }
