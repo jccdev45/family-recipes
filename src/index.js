fb85eddb78b9e1ab10cad0aea838e19d33b45901
JordanCruzCorrea <jccdev45@gmail.com>
2021-05-20 05:24:18 -0400

    Basic PWA setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import App from "./App";
 import "./styles/tailwind.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
