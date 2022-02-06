d0b94c3ba73d69375775ad0494226c497a4e5d7a
jccdev45 <jccdev45@gmail.com>
2022-02-06 04:01:35 -0500

    Minor design changes

--- src/screens/recipes/RecipeDetails.js
+++ src/screens/recipes/RecipeDetails.js
@@ -50,7 +50,8 @@ export function RecipeDetails() {
         return (
           <Link
             to={`/edit-recipe/${recipe.id}`}
-            className="mx-auto my-2 text-white bg-blue-400 btn hover:bg-blue-500"
+            // inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg
+            className="px-6 py-2 mx-auto my-2 text-white bg-blue-400 border-0 focus:outline-none btn hover:bg-blue-500"
           >
             Edit Recipe
             {/* <Button text="Edit Recipe" /> */}
@@ -88,8 +89,40 @@ export function RecipeDetails() {
 
     return (
       <div className="flex flex-col justify-center w-full rounded">
+        <section className="text-gray-600 body-font">
+          <div className="container flex flex-col items-center px-5 py-24 mx-auto md:flex-row">
+            <div className="w-5/6 mb-10 lg:max-w-lg md:w-1/3 md:mb-0">
+              <img
+                className="object-cover object-center rounded aspect-square"
+                src={img}
+                alt={recipeName}
+              />
+            </div>
+            <div className="flex flex-col items-center text-center lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start md:text-left">
+              <h1 className="mb-4 text-3xl font-medium text-gray-900 title-font sm:text-4xl">
+                {recipeName}{" "}
+                <span>
+                  <br className="md:hidden" />
+                  <Link
+                    to={`/user/${userId}`}
+                    className="w-full text-xl text-center"
+                  >
+                    <span>by:</span>
+                    <span className="mx-1 text-blue-300 hover:underline">
+                      {author}
+                    </span>
+                  </Link>
+                </span>
+                <br className="lg:inline-block" />
+                <span className="text-xl">"{quote}"</span>
+              </h1>
+              <div className="flex justify-center">{renderEdit()}</div>
+            </div>
+          </div>
+        </section>
+
         {/* INFO */}
-        <div className="grid w-11/12 grid-cols-1 px-2 py-2 mx-auto my-2 bg-red-100 rounded-lg bg-opacity-30 lg:px-12 md:w-5/6 lg:w-2/3 md:grid-cols-2">
+        {/* <div className="grid w-11/12 grid-cols-1 px-2 py-2 mx-auto my-2 bg-red-100 rounded-lg bg-opacity-30 lg:px-12 md:w-5/6 lg:w-2/3 md:grid-cols-2">
           <div className="flex flex-col items-center order-2 w-full p-4 m-auto">
             <h1 className="w-full my-4 text-2xl font-semibold text-center md:text-3xl lg:text-4xl">
               {recipeName}
@@ -111,7 +144,7 @@ export function RecipeDetails() {
             alt={recipeName}
             className="w-full rounded-lg lg:w-2/3"
           />
-        </div>
+        </div> */}
         <ul className="flex items-center justify-center my-4">
           {tags && renderTags()}
         </ul>
